1f68b82c92eab2f81ac0c8f66c9888d40e0cc7c6
Alexander Jones <spiderland@outlook.com>
2023-06-05 21:31:25 +0800

    tweak formatting add reset btn

--- src/components/VendorFilter.tsx
+++ src/components/VendorFilter.tsx
@@ -13,6 +13,7 @@ const VendorFilter = () => {
     const vendors = Object.values(VENDORS).flatMap(e => Object.values(e)).sort((a, b) => a.name === b.name ? 0 : a.name < b.name ? -1 : 1);
     const [ignore, setIgnore] = useLocalStorage<Record<string, boolean>>("fs-ignore-vendors", {});
     const [transientIgnore, setTransientIgnore] = useState<Record<string, boolean>>(ignore)
+    const hasChanges = useRef<boolean>(false)
 
     const handleCheck = (checked: boolean, url: string) => {
 
@@ -24,30 +25,33 @@ const VendorFilter = () => {
         }
 
         setTransientIgnore(newIgnore);
+        hasChanges.current = true;
     }
 
     const onReset = () => {
 
         setTransientIgnore({});
+        hasChanges.current = true;
     }
 
     const onSave = () => {
 
         setIgnore(transientIgnore);
-    }
-
-    useEffect(() => {
+        hasChanges.current = false;
         close();
-    }, [ignore])
+    }
 
     const close = () => {
 
-        setTransientIgnore(ignore);
+        // Needed to avoid setting stale state.
+        if (hasChanges.current) {
+            setTransientIgnore(ignore);
+            hasChanges.current = false;
+        }
         onClose();
     }
 
     return (
-        
         <>
         <IconButton
             size="md"
@@ -57,18 +61,27 @@ const VendorFilter = () => {
             marginLeft="2"
             onClick={onOpen}
             icon={<MdStore />}
-            aria-label={`Set vendor options`} />
-        <BasicModal isOpen={isOpen} onReset={onReset} onSave={onSave} onClose={close} title="Vendor options">
-            <VStack wrap={"wrap"} width="100%" maxH={"20%"} alignItems={"baseline"}>
+            aria-label={`Set vendor options`} 
+        />
+        <BasicModal 
+            isOpen={isOpen} 
+            reset={{text: "Select all", action: onReset}} 
+            onSave={onSave} 
+            onClose={close} 
+            title="Vendor options"
+        >
+            <VStack>
                 <Text>
                     The following vendors are currently supported. If a particular vendor isn't
                     relevant to you, you may exclude them from the search results here.
                 </Text>
-                {vendors.map(e => (
-                <Checkbox key={e.name} onChange={ev => handleCheck(ev.target.checked, e.url)} isChecked={!transientIgnore[e.url]}>
-                    {e.name}
-                </Checkbox>
-                ))}
+                <HStack wrap={"wrap"} alignItems={"baseline"} justifyContent={"space-around"}>
+                    {vendors.map(e => (
+                    <Checkbox flexBasis={"45%"}  key={e.name} onChange={ev => handleCheck(ev.target.checked, e.url)} isChecked={!transientIgnore[e.url]}>
+                        {e.name}
+                    </Checkbox>
+                    ))}
+                </HStack>
             </VStack>
         </BasicModal>
         </>
--- src/components/modal/BasicModal.tsx
+++ src/components/modal/BasicModal.tsx
@@ -3,14 +3,14 @@ import { Button, Flex, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFo
 export type BasicModalProps = {
 
   isOpen: boolean,
-  onReset?: () => void,
+  reset?: {text: string, action: () => void},
   onSave?: () => void,
   onClose: () => void,
   title: string,
   children: React.ReactElement | React.ReactElement[]
 }
 
-export const BasicModal: React.FC<BasicModalProps> = ({ isOpen, onReset, onSave, onClose, title, children }) => {
+export const BasicModal: React.FC<BasicModalProps> = ({ isOpen, reset, onSave, onClose, title, children }) => {
     return (
         <>
         <Modal isOpen={isOpen} onClose={onClose}>
@@ -22,9 +22,9 @@ export const BasicModal: React.FC<BasicModalProps> = ({ isOpen, onReset, onSave,
               {children}
             </ModalBody>
             <ModalFooter>
-                {onReset &&
-                <Button colorScheme='whiteAlpha' mr={"auto"} onClick={onReset}>
-                  Reset
+                {reset &&
+                <Button colorScheme='whiteAlpha' mr={"auto"} onClick={reset.action}>
+                  {reset.text}
                 </Button>}
                 <Button colorScheme='blue' mr={3} onClick={onSave || onClose}>
                   Save
