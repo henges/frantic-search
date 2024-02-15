7ee09b87ccde5367191302cd1275ac9a2d6ea2f9
pnametala <pnametala@gmail.com>
2024-02-15 08:56:54 +0800

    Added Plenty of Games as a Shopify store (#2)

--- src/types/Vendors.ts
+++ src/types/Vendors.ts
@@ -63,6 +63,12 @@ export const BINDER_POS_VENDORS: Record<string, BinderPosBackendVendor> = {
         url: "unplugged-games.myshopify.com",
         name: "Unplugged Games"
     }
+    ,
+    "plenty-of-games-au.myshopify.com": {
+        backend: VendorBackend.BINDER_POS,
+        url: "plenty-of-games-au.myshopify.com",
+        name: "Plenty of Games"
+    }
 }
 export const MTGMATE_VENDORS: Record<string, MtgMateVendor> = {
     "mtgmate.com.au": {
